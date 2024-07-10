c37dfd677538979057f2609ecfb34b768d2916bf
Orlov Viacheslav <viacheslavorlov@gmail.com>
2024-07-10 17:00:24 +0300

    todos added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
-import node from "@astrojs/node";
-import compress from "astro-compress";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
+import node from '@astrojs/node';
+import compress from 'astro-compress';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  security: {
-    checkOrigin: true
-  },
-  site: 'http://astro-template.ru', // TODO
-  integrations: [tailwind(), preact(), mdx(), sitemap(), compress()],
-  adapter: node({
-    mode: "standalone"
-  })
+	output: 'server',
+	security: {
+		checkOrigin: true,
+	},
+	site: 'http://astro-template.ru', // TODO
+	integrations: [tailwind(), preact(), mdx(), sitemap(), compress()],
+	adapter: node({
+		mode: 'standalone', //todo
+	}),
 });
