3bcd10aed0b8d54c6fbe1c4b6e9845ba76d0eda3
Orlov Viacheslav <viacheslavorlov@gmail.com>
2024-07-10 16:50:15 +0300

    Добавление базовой информации в папке content

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,4 +2,4 @@ export type Align = 'left' | 'right' | 'center';
 export type ElementAlign = 'left' | 'right' | 'center';
 export type Variant = 'dark' | 'light' | 'medium';
 export type Size = 'small' | 'big'
-export type H = '3xl' | '2xl' | 'xl' | 'l' | 'm';
+export type H = 'xl' | 'l' | 'm';
