a1256c3c3bc4c3152b0bfd705c068577dc025f65
Viacheslav <viacheslavorlov@gmail.com>
2024-12-09 23:43:19 +0300

    strapi-integration

--- src/shared/lib/strapi.ts
+++ src/shared/lib/strapi.ts
@@ -1,9 +1,40 @@
+import qs from "qs";
+import type { components, paths } from "#/types/strapi/strapi"; // create types by `npm run types:generate`
+
+interface Query {
+  /** @description Sort by attributes ascending (asc) or descending (desc) */
+  sort?: string;
+  /** @description Return page/pageSize (default: true) */
+  "pagination[withCount]"?: boolean;
+  /** @description Page number (default: 0) */
+  "pagination[page]"?: number;
+  /** @description Page size (default: 25) */
+  "pagination[pageSize]"?: number;
+  /** @description Offset value (default: 0) */
+  "pagination[start]"?: number;
+  /** @description Number of entities to return (default: 25) */
+  "pagination[limit]"?: number;
+  /** @description Fields to return (ex: title,author) */
+  fields?: string;
+  /** @description Relations to return */
+  populate?: string | string[];
+  /** @description Filters to apply */
+  filters?: {
+    [key: string]: unknown;
+  };
+  /** @description Locale to apply */
+  locale?: string;
+}
+
 interface Props {
-  endpoint: string;
-  query?: Record<string, string>;
+  endpoint: keyof paths;
+  query?: Query;
   wrappedByKey?: string;
   wrappedByList?: boolean;
+  token?: string;
 }
+
+type retu = ({} & keyof components["schemas"]) | components["schemas"];
 
 /**
  * Fetches data from the Strapi API
@@ -11,26 +42,37 @@ interface Props {
  * @param query - The query parameters to add to the url
  * @param wrappedByKey - The key to unwrap the response from
  * @param wrappedByList - If the response is a list, unwrap it
+ * @param token - if token exist Bearer authorization continues
  * @returns
  */
-export default async function fetchApi<T>({
+export default async function fetchApi<retu>({
   endpoint,
   query,
   wrappedByKey,
   wrappedByList,
-}: Props): Promise<T> {
+  token,
+}: Props): Promise<retu> {
   if (endpoint.startsWith("/")) {
-    endpoint = endpoint.slice(1);
+    endpoint = endpoint.slice(1) as Props["endpoint"];
   }
 
-  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
-
+  const url = new URL(`${import.meta.env.PUBLIC_STRAPI_URL}/api/${endpoint}`);
+  let fullUrl: string | undefined;
   if (query) {
-    Object.entries(query).forEach(([key, value]) => {
-      url.searchParams.append(key, value);
-    });
+    fullUrl = `${url.toString()}?${qs.stringify(query, {
+      encodeValuesOnly: true,
+      charset: "utf-8", // prettify URL
+    })}`;
+    // Object.entries(query).forEach(([key, value]) => {
+    //   url.searchParams.append(key, value);
+    // });
   }
-  const res = await fetch(url.toString());
+  const headers = {
+    "Content-type": "application/json",
+  };
+  if (token) Object.assign(headers, { Authorization: `Bearer ${token}` });
+
+  const res = await fetch((fullUrl || url).toString(), { headers });
   let data = await res.json();
 
   if (wrappedByKey) {
@@ -41,5 +83,5 @@ export default async function fetchApi<T>({
     data = data[0];
   }
 
-  return data as T;
+  return data as retu;
 }
