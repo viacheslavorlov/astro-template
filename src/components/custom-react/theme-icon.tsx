b93cad040725603310d26b5aa15890836840ff4f
viacheslav <viacheslavorlov@gmail.com>
2025-10-22 00:50:16 +0300

    react theme toggle component

---
+++ src/components/custom-react/theme-icon.tsx
@@ -0,0 +1,41 @@
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+export function ThemeIcon() {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  useEffect(() => {
+    const initialTheme = (() => {
+      const localStorageTheme = localStorage?.getItem("theme") ?? "";
+      if (["dark", "light"].includes(localStorageTheme)) {
+        return localStorageTheme as Theme;
+      }
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark";
+      }
+      return "light";
+    })();
+    setTheme(initialTheme);
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <button onClick={toggleTheme} aria-label="Toggle theme">
+      {theme === "light" ? <Sun /> : <Moon />}
+    </button>
+  );
+}
